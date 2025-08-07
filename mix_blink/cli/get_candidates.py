02e5326f1f126c9de1037a77efa534881fe516e3
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-08-07 18:49:02 +0900

    add get_candidates

--- mix_blink/cli/get_candidates.py
+++ mix_blink/cli/get_candidates.py
@@ -1,8 +1,10 @@
+import json
 import logging
 import os
 from pathlib import Path
 
 import torch
+from datasets import Dataset
 from datasets.fingerprint import get_temporary_cache_files_directory
 from transformers import (
     AutoTokenizer,
@@ -22,7 +24,41 @@ from mix_blink.retriever import BM25Retriever, DenseRetriever
 from mix_blink.training import setup_logger
 
 logger = logging.getLogger(__name__)
-TOKEN = os.environ.get("TOKEN", True)
+
+def add_candidate_ids(dataset: Dataset, candidate_ids: list[list[str]]) -> list[dict]:
+    added_dataset = []
+    for data in dataset:
+        examples = {"id": data["id"], "examples": []}
+        for example in data["examples"]:
+            entities = example["entities"]
+            example = {"id": example["id"], "text": example["text"], "entities": []}
+            for entity in entities:
+                candidates = candidate_ids.pop(0)
+                example["entities"].append({
+                    "start": entity["start"],
+                    "end": entity["end"],
+                    "label": entity["label"],
+                    "title": entity["title"],
+                    "text": entity["text"],
+                    "candidates": candidates
+                })
+            examples["examples"].append(example)
+        added_dataset.append(examples)
+    assert not candidate_ids
+    return added_dataset
+
+
+def write_jsonl(dataset: list[dict], output_path: str) -> None:
+    """
+    Write a dataset to a JSONL file.
+
+    Args:
+        dataset (list[dict]): The dataset to write.
+        output_path (str): The path to the output JSONL file.
+    """
+    with open(output_path, 'w', encoding='utf-8') as f:
+        for data in dataset:
+            f.write(json.dumps(data, ensure_ascii=False) + '\n')
 
 
 def main(data_args: DatasetArguments, model_args: ModelArguments, training_args: TrainingArguments) -> None:
@@ -42,8 +78,8 @@ def main(data_args: DatasetArguments, model_args: ModelArguments, training_args:
         model = MixBlink.from_pretrained(model_args.prev_path)
         config = model.config
     else:
-        mention_tokenizer = AutoTokenizer.from_pretrained(model_args.mention_encoder, model_max_length=model_args.mention_context_length, token=TOKEN)
-        entity_tokenizer = AutoTokenizer.from_pretrained(model_args.entity_encoder, model_max_length=model_args.entity_context_length, token=TOKEN)
+        mention_tokenizer = AutoTokenizer.from_pretrained(model_args.mention_encoder, model_max_length=model_args.mention_context_length, token=True)
+        entity_tokenizer = AutoTokenizer.from_pretrained(model_args.entity_encoder, model_max_length=model_args.entity_context_length, token=True)
         mention_tokenizer.add_tokens(data_args.start_mention_token, data_args.end_mention_token)
         entity_tokenizer.add_tokens([data_args.entity_token, data_args.nil_label])
         config = MixBlinkConfig(
@@ -58,6 +94,12 @@ def main(data_args: DatasetArguments, model_args: ModelArguments, training_args:
         model = MixBlink(config)
 
     cache_dir = model_args.cache_dir or get_temporary_cache_files_directory()
+    raw_datasets = read_dataset(
+        data_args.train_file,
+        data_args.validation_file,
+        data_args.test_file,
+        cache_dir
+    )
     dictionary = EntityDictionary(
         tokenizer=entity_tokenizer,
         dictionary_path=data_args.dictionary_file,
@@ -65,13 +107,6 @@ def main(data_args: DatasetArguments, model_args: ModelArguments, training_args:
         cache_dir=cache_dir,
         training_arguments=training_args,
         nil={"name": data_args.nil_label, "description": data_args.nil_description} if data_args.add_nil else None
-    )
-
-    raw_datasets = read_dataset(
-        data_args.train_file,
-        data_args.validation_file,
-        data_args.test_file,
-        cache_dir
     )
     preprocessor = Preprocessor(
         mention_tokenizer,
@@ -91,13 +126,16 @@ def main(data_args: DatasetArguments, model_args: ModelArguments, training_args:
         bm25_retriever.deserialize_from(os.path.join(training_args.output_dir, 'retriever_bm25'))
         if 'train' in splits:
             candidate_ids = bm25_retriever.get_hard_negatives(splits['train'], n_threads=training_args.dataloader_num_workers)
-            splits['train'] = splits['train'].add_column("negatives", candidate_ids)
+            train_data = add_candidate_ids(raw_datasets['train'], candidate_ids)
+            write_jsonl(train_data, data_args.train_file.replace('.jsonl', '_candidates_bm25.jsonl'))
         if 'validation' in splits:
             candidate_ids = bm25_retriever.get_hard_negatives(splits['validation'], n_threads=training_args.dataloader_num_workers)
-            splits['validation'] = splits['validation'].add_column("negatives", candidate_ids)
+            dev_data = add_candidate_ids(raw_datasets['validation'], candidate_ids)
+            write_jsonl(dev_data, data_args.validation_file.replace('.jsonl', '_candidates_bm25.jsonl'))
         if 'test' in splits:
             candidate_ids = bm25_retriever.get_hard_negatives(splits['test'], n_threads=training_args.dataloader_num_workers)
-            splits['test'] = splits['test'].add_column("negatives", candidate_ids)
+            test_data = add_candidate_ids(raw_datasets['test'], candidate_ids)
+            write_jsonl(test_data, data_args.test_file.replace('.jsonl', '_candidates_bm25.jsonl'))
     elif model_args.negative == 'dense':
         retriever = DenseRetriever(
             entity_tokenizer,
@@ -112,13 +150,16 @@ def main(data_args: DatasetArguments, model_args: ModelArguments, training_args:
         )
         if 'train' in splits:
             train_candidate_ids = retriever.get_hard_negatives(model, splits['train'])
-            splits['train'] = splits['train'].add_column("negatives", train_candidate_ids)
+            train_data = add_candidate_ids(raw_datasets['train'], train_candidate_ids)
+            write_jsonl(train_data, data_args.train_file.replace('.jsonl', '_candidates_dense.jsonl'))
         if 'validation' in splits:
             dev_candidate_ids = retriever.get_hard_negatives(model, splits['validation'])
-            splits['validation'] = splits['validation'].add_column("negatives", dev_candidate_ids)
+            dev_data = add_candidate_ids(raw_datasets['validation'], dev_candidate_ids)
+            write_jsonl(dev_data, data_args.validation_file.replace('.jsonl', '_candidates_dense.jsonl'))
         if 'test' in splits:
             test_candidate_ids = retriever.get_hard_negatives(model, splits['test'])
-            splits['test'] = splits['test'].add_column("negatives", test_candidate_ids)
+            test_data = add_candidate_ids(raw_datasets['test'], test_candidate_ids)
+            write_jsonl(test_data, data_args.test_file.replace('.jsonl', '_candidates_dense.jsonl'))
 
 
 def cli_main() -> None:
