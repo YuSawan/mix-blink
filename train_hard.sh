a18c94b7950aac4ac6eecb14d385e8f859f3ddf5
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-08-15 00:09:16 +0900

    modify

---
+++ train_hard.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=MixBLINK_train
+#SBATCH -p gpu_long
+#SBATCH --gres=gpu:a6000:1
+
+export WANDB_PROJECT=MixBLINK_EL
+
+config_file=configs/config_hard.yaml
+output_dir=save_models/bert-base-uncased
+seed=0
+measure='ip'
+
+base_output_dir=${output_dir}/${seed}/${measure}
+mkdir -p ${base_output_dir}
+
+uv run torchrun mix_blink/cli/run.py \
+    --config_file ${config_file} \
+    --measure ${measure} \
+    --output_dir ${base_output_dir}/inbatch \
+    --run_name ${base_output_dir}/inbatch \
+    --seed ${seed} \
+    --hard_negative
